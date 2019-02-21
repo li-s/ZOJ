518e67025656f5863aaff8cbf3a372ac628d894a
li-s <lishuo980@gmail.com>
2019-02-21 10:39:09 +0800

    increase performance of 1005.py

--- Vol 1/1005.py
+++ Vol 1/1005.py
@@ -1,12 +1,12 @@
 import sys
 
-def pour(ca, cb, n):
+def pourba(ca, cb, n):
     a = 0
     b = cb
     print 'fill B'
     while b != n:
         print 'pour B A'
-        if ca - a < b:
+        if (ca - a) < b:
             print 'empty A'
             b = b - ca + a
             a = 0
@@ -17,11 +17,34 @@ def pour(ca, cb, n):
     print 'success'
     return
 
+def pourab(ca, cb, n):
+    a = ca
+    b = 0
+    print "fill A"
+    while a != n:
+        print "pour A B"
+        if (cb - b) < a:
+            print "empty B"
+            a = a - cb + b
+            b = 0
+        else:
+            print "fill A"
+            b = b + a
+            a = ca
+    print "success"
+    return
+
 line = sys.stdin.readline()
 while line:
     data = line.split()
     ca = int(data[0])
     cb = int(data[1])
     n = int (data[2])
-    pour(ca, cb, n)
+
+    if (n % 2) == 0:
+        pourba(ca, cb, n)
+
+    else:
+        pourab(ca, cb, n)
+
     line = sys.stdin.readline()
